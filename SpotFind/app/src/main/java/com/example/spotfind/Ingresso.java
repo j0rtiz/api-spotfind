6c1c151b58264e06b5a08f1629ae91c8f50e70b1
Fabiano M B Dutra <fmbdutra@gmail.com>
2019-11-24 21:55:28 -0300

    classe ingresso

---
+++ SpotFind/app/src/main/java/com/example/spotfind/Ingresso.java
@@ -0,0 +1,87 @@
+package com.example.spotfind;
+
+public class Ingresso {
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getAno() {
+    return ano;
+  }
+
+  public void setAno(String ano) {
+    this.ano = ano;
+  }
+
+  public String getGenero() {
+    return genero;
+  }
+
+  public void setGenero(String genero) {
+    this.genero = genero;
+  }
+
+  public String getAtores() {
+    return atores;
+  }
+
+  public void setAtores(String atores) {
+    this.atores = atores;
+  }
+
+  public String getPoster() {
+    return poster;
+  }
+
+  public void setPoster(String poster) {
+    this.poster = poster;
+  }
+
+  public String getImbdId() {
+    return imbdId;
+  }
+
+  public void setImbdId(String imbdId) {
+    this.imbdId = imbdId;
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
+
+  public String getLocal() {
+    return local;
+  }
+
+  public void setLocal(String local) {
+    this.local = local;
+  }
+
+  public Integer getValor() {
+    return valor;
+  }
+
+  public void setValor(Integer valor) {
+    this.valor = valor;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  String titulo, ano, genero, atores, poster, imbdId, tipo, local;
+  Integer valor, id;
+}
